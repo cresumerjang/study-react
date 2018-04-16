e26e3f6bd9dba3da324810beb983ac3c3cb7f8fa
Jang Jae Won <jaewjang@ebay.com>
2018-04-16 11:20:19 +0900

    asdf

--- todo_list_test/src/store/index.js
+++ todo_list_test/src/store/index.js
@@ -3,8 +3,8 @@ import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
-import todosFilter from '../modules/todoList/actionAndReducer/filter';
-import todos from '../modules/todoList/actionAndReducer/todos';
+import todosFilter from '../modules/todoList/actionAndReducers/filter';
+import todos from '../modules/todoList/actionAndReducers/todos';
 
 const logger = createLogger({/* timestamp: true, diff:true */});
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
